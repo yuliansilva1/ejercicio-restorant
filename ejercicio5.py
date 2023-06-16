2a6c481444ba5846690bb4ad678dd55886ea3d9d
yuliansilva1 <136838106+yuliansilva1@users.noreply.github.com>
2023-06-16 12:45:27 -0400

    Add files via upload

---
+++ ejercicio5.py
@@ -0,0 +1,13 @@
+#ejercicio 5
+import os 
+import time
+import msvcrt
+
+#limpiar pantalla:
+os.system('cls')
+
+#esperar cierta cantidad de segundos:
+time.sleep(3)
+
+#esperar tecla:
+msvcrt.getch()
